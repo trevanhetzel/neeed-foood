bb059637e702660bb11f260a3755fedf47ace387
Trevan Hetzel <trevan@hetzelcreative.com>
2014-08-28 22:11:40 -0500

    JS and styles for sentence filter dropdowns

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 (function ($) {
+	var self = window.NF = window.NF || {};
 
 	// Yummly info
 	var yummlyID = '216cd33e',
@@ -9,30 +10,41 @@
 	var $search = $('#search'),
 		$searchCourse = $('#searchCourse'),
 		$searchCuisine = $('#searchCuisine'),
-		$mealList = $('.meal-list');
+		$mealList = $('.meal-list'),
+		$filterTrigger = $('.sf-trigger'),
+		$filterContain = $('.sf-drop-contain');
 
-	// Construct NeeedFoood class
-    var NeeedFoood = function () {
-    	var self = this;
+	// Register events
+	self.registerEvents = function () {
+		$filterTrigger.on('click', self.sentenceFilter);
+		$search.on('submit', self.doSearch);
+	};
 
-    	$search.on('submit', function (e) {
-    		// Prevent from from submitting
-    		e.preventDefault();
+	self.sentenceFilter = function (e) {
+		e.preventDefault();
+		var $this = $(this),
+			$filterParent = $this.parent('.sf-drop-contain');
+		
+		$filterParent.toggleClass('sf-drop--open');
+	};
 
-    		// Grab user submitted values
-    		var courseVal = $searchCourse.val(),
-    			cuisineVal = $searchCuisine.val();
+	// Grab some things needed for our search
+    self.doSearch = function (e) {
+    	// Prevent from from submitting
+		e.preventDefault();
 
-    		// Do the search using value of input
-    		if (courseVal !== 'default' && cuisineVal !== 'default') {
-    			self.doSearch(courseVal, cuisineVal);
-    		}
-    	});
+		// Grab user submitted values
+		var courseVal = $searchCourse.val(),
+			cuisineVal = $searchCuisine.val();
+
+		// Do the search using value of input
+		if (courseVal !== 'default' && cuisineVal !== 'default') {
+			self.doRequest(courseVal, cuisineVal);
+		}
     };
 
-    NeeedFoood.prototype.doSearch = function (courseVal, cuisineVal) {
-    	var self = this;
-
+    // Hit the Yummly API and grab recipes
+    self.doRequest = function (courseVal, cuisineVal) {
     	var searchRequest = $.ajax({
 			url: 'http://api.yummly.com/v1/api/recipes?_app_id=' + yummlyID + '&_app_key=' + yummlyKey + '&allowedCourse[]=course^course-' + courseVal + '&allowedCuisine[]=cuisine^cuisine-' + cuisineVal + '&maxResult=' + numResults,
 			type: 'GET',
@@ -51,9 +63,8 @@
 		});
     };
 
-    NeeedFoood.prototype.fetchRecipe = function (recipeID) {
-    	var self = this;
-
+    // Grab the individual recipes and spit them out
+    self.fetchRecipe = function (recipeID) {
     	var recipeRequest = $.ajax({
 			url: 'http://api.yummly.com/v1/api/recipe/' + recipeID + '?_app_id=' + yummlyID + '&_app_key=' + yummlyKey,
 			type: 'GET',
@@ -65,7 +76,6 @@
 		});
     };
 
-    // Create NeeedFoood instance
-    new NeeedFoood();
+    self.registerEvents();
 
-})(jQuery);
+}) (jQuery);
